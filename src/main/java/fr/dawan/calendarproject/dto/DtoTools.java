5c1cca70f43be3f54c48fbea7ddeb7f22b9c1be8
Romain Maury <rmaury@dawan.fr>
2021-05-05 12:05:06 +0200

    update DtoTools for converting object list to id list

--- src/main/java/fr/dawan/calendarproject/dto/DtoTools.java
+++ src/main/java/fr/dawan/calendarproject/dto/DtoTools.java
@@ -1,5 +1,11 @@
 package fr.dawan.calendarproject.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 
 import fr.dawan.calendarproject.entities.Intervention;
@@ -10,25 +16,49 @@ public class DtoTools {
 
 	public static <TSource, TDestination> TDestination convert(TSource obj, Class<TDestination> clazz) {
 		ModelMapper myMapper = new ModelMapper();
-		
-		myMapper.typeMap(Intervention.class, InterventionDto.class)
-			.addMappings(mapper->{
-				mapper.map(src->src.getUser().getId(), InterventionDto::setUserId);
-				mapper.map(src->src.getLocation().getId(), InterventionDto::setLocationId);
-				mapper.map(src->src.getCourse().getId(), InterventionDto::setCourseId);
-				mapper.map(src->src.getCourse().getId(), InterventionDto::setCourseId);
-			});
 
-		myMapper.typeMap(User.class, AdvancedUserDto.class)
-			.addMappings(mapper -> {
-				mapper.map(src->src.getSkills().stream().map(s -> s.getId()).toArray(), AdvancedUserDto::setSkillsId);
-			});
-		
-		myMapper.typeMap(Skill.class, AdvancedSkillDto.class)
-		.addMappings(mapper -> {
-			mapper.map(src->src.getUsers().stream().map(s -> s.getId()).toArray(), AdvancedSkillDto::setUsersId);
+		myMapper.typeMap(Intervention.class, InterventionDto.class).addMappings(mapper -> {
+			mapper.map(src -> src.getUser().getId(), InterventionDto::setUserId);
+			mapper.map(src -> src.getLocation().getId(), InterventionDto::setLocationId);
+			mapper.map(src -> src.getCourse().getId(), InterventionDto::setCourseId);
+			mapper.map(src -> src.getCourse().getId(), InterventionDto::setCourseId);
 		});
 
+		Converter<Set<Skill>, List<Long>> skillsToIdsList = new AbstractConverter<Set<Skill>, List<Long>>() {
+			@Override
+			protected List<Long> convert(Set<Skill> context) {
+				List<Long> ids = new ArrayList<Long>();
+				if (context.isEmpty())
+					return null;
+				else {
+					context.forEach(s -> ids.add(s.getId()));
+					return ids;
+				}
+			}
+		};
+
+		myMapper.typeMap(User.class, AdvancedUserDto.class).addMappings(mapper -> {
+			mapper.using(skillsToIdsList).map(User::getSkills, AdvancedUserDto::setSkillsId);
+		});
+
+
+		Converter<Set<User>, List<Long>> usersToIdsList = new AbstractConverter<Set<User>, List<Long>>() {
+			@Override
+			protected List<Long> convert(Set<User> context) {
+				List<Long> ids = new ArrayList<Long>();
+				if (context.isEmpty())
+					return null;
+				else {
+					context.forEach(u -> ids.add(u.getId()));
+					return ids;
+				}
+			}
+		};
+		
+		myMapper.typeMap(Skill.class, AdvancedSkillDto.class).addMappings(mapper -> {
+			mapper.using(usersToIdsList).map(Skill::getUsers, AdvancedSkillDto::setUsersId);
+		});
+		
 		return myMapper.map(obj, clazz);
 	}
 }
