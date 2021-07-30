e4912f2be3cc35359a7e70a6eed0ebdca20d30ff
Romain Maury <rmaury@dawan.fr>
2021-07-30 12:35:19 +0200

    add uniqness check for location city and color

--- src/main/java/fr/dawan/calendarproject/repositories/LocationRepository.java
+++ src/main/java/fr/dawan/calendarproject/repositories/LocationRepository.java
@@ -1,6 +1,10 @@
 package fr.dawan.calendarproject.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import fr.dawan.calendarproject.entities.Location;
@@ -8,4 +12,6 @@ import fr.dawan.calendarproject.entities.Location;
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
+	@Query("FROM Location l WHERE l.id IS NOT :id AND l.color = :color OR l.city = :city")
+	List<Location> findDuplicates(@Param("id") long id, @Param("city") String city, @Param("color") String color);
 }
--- src/main/java/fr/dawan/calendarproject/services/LocationServiceImpl.java
+++ src/main/java/fr/dawan/calendarproject/services/LocationServiceImpl.java
@@ -1,8 +1,10 @@
 package fr.dawan.calendarproject.services;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
@@ -11,9 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import fr.dawan.calendarproject.dto.APIError;
 import fr.dawan.calendarproject.dto.DtoTools;
 import fr.dawan.calendarproject.dto.LocationDto;
 import fr.dawan.calendarproject.entities.Location;
+import fr.dawan.calendarproject.exceptions.EntityFormatException;
 import fr.dawan.calendarproject.repositories.LocationRepository;
 
 @Service
@@ -60,6 +64,8 @@ public class LocationServiceImpl implements LocationService {
 
 	@Override
 	public LocationDto saveOrUpdate(LocationDto locationDto) {
+		checkUniqness(locationDto);
+		
 		if (locationDto.getId() > 0 && !locationRepository.existsById(locationDto.getId()))
 			return null;
 		Location l = DtoTools.convert(locationDto, Location.class);
@@ -72,6 +78,32 @@ public class LocationServiceImpl implements LocationService {
 	public LocationDto count() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public boolean checkUniqness(LocationDto location) {
+		System.out.println("id ::: " + location.getId());
+		System.out.println("city ::: " + location.getCity());
+		System.out.println("color ::: " + location.getColor());
+		List<Location> duplicates = locationRepository.findDuplicates(location.getId(), location.getCity(), location.getColor());
+		
+		System.out.println("dup size ::: " + duplicates.size());
+		if (duplicates.size() > 0) {
+			Set<APIError> errors = new HashSet<APIError>();
+			String instanceClass = location.getClass().toString();
+			String path = "/api/locations";
+			
+			for (Location loc : duplicates) {
+				if (loc.getCity().equals(location.getCity()))
+					errors.add(new APIError(505, instanceClass, path, "Location with name " + location.getCity() + " already exists", path));
+				if (loc.getColor().equals(location.getColor()))
+					errors.add(new APIError(505, instanceClass, path, "Location with color " + location.getColor() + " already exists", path));
+			}
+			
+			throw new EntityFormatException(errors);
+		}
+		
+		return true;
 	}
 
 }
