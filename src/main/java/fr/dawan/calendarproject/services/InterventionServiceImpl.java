dec2cb7554ae6c4cd02a78b3eb0da78f27f9c1d8
Romain Maury <rmaury@dawan.fr>
2021-08-30 13:49:20 +0200

    implement spy mock

--- src/main/java/fr/dawan/calendarproject/services/InterventionServiceImpl.java
+++ src/main/java/fr/dawan/calendarproject/services/InterventionServiceImpl.java
@@ -73,6 +73,7 @@ public class InterventionServiceImpl implements InterventionService {
 		for (Intervention intervention : interventions) {
 			interventionsDto.add(DtoTools.convert(intervention, InterventionDto.class));
 		}
+		
 		return interventionsDto;
 	}
 
@@ -134,20 +135,25 @@ public class InterventionServiceImpl implements InterventionService {
 			interv.setMasterIntervention(interventionRepository.getOne(intervention.getMasterInterventionId()));
 		
 		interv = interventionRepository.saveAndFlush(interv);
-
+		saveMemento(interv);
+		
+		return DtoTools.convert(interv, InterventionDto.class);
+	}
+	
+	public InterventionMemento saveMemento(Intervention interv) {
 		// Memento creation
 		// Build interventionMemento object
 		InterventionMemento intMemento = new InterventionMemento();
 		intMemento.setState(DtoTools.convert(interv, InterventionMementoDto.class));
 		// Save memento
 		try {
-			caretaker.addMemento(intervention.getId(), "test", intMemento.createMemento());
+			caretaker.addMemento(interv.getId(), "test", intMemento.createMemento());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		interventionMementoRepository.saveAndFlush(intMemento);
-		return DtoTools.convert(interv, InterventionDto.class);
+		
+		return interventionMementoRepository.saveAndFlush(intMemento);
 	}
 
 	// Search
