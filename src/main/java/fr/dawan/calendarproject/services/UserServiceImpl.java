88d4432dcce370c917bf7f0f4aa854c5c91e9844
Ahmed Reda Mokhtari <amokhtari@dawan.fr>
2021-09-01 16:07:34 +0200

    Mapper with Mapstruct

--- src/main/java/fr/dawan/calendarproject/services/UserServiceImpl.java
+++ src/main/java/fr/dawan/calendarproject/services/UserServiceImpl.java
@@ -16,13 +16,13 @@ import org.springframework.stereotype.Service;
 import fr.dawan.calendarproject.dto.APIError;
 import fr.dawan.calendarproject.dto.AdvancedUserDto;
 import fr.dawan.calendarproject.dto.DtoTools;
-import fr.dawan.calendarproject.dto.LocationDto;
-import fr.dawan.calendarproject.entities.Location;
 import fr.dawan.calendarproject.entities.Skill;
 import fr.dawan.calendarproject.entities.User;
 import fr.dawan.calendarproject.enums.UserCompany;
 import fr.dawan.calendarproject.enums.UserType;
 import fr.dawan.calendarproject.exceptions.EntityFormatException;
+import fr.dawan.calendarproject.mapper.DtoMapper;
+import fr.dawan.calendarproject.mapper.DtoMapperImpl;
 import fr.dawan.calendarproject.repositories.LocationRepository;
 import fr.dawan.calendarproject.repositories.SkillRepository;
 import fr.dawan.calendarproject.repositories.UserRepository;
@@ -37,12 +37,14 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private LocationRepository locationRepository;
-	
+
 	@Autowired
 	private LocationService locationService;
 
 	@Autowired
 	private SkillRepository skillRepository;
+
+	private DtoMapper mapper = new DtoMapperImpl();
 
 	@Override
 	public List<AdvancedUserDto> getAllUsers() {
@@ -50,20 +52,21 @@ public class UserServiceImpl implements UserService {
 		List<AdvancedUserDto> result = new ArrayList<AdvancedUserDto>();
 
 		for (User u : users) {
-			result.add(DtoTools.convert(u, AdvancedUserDto.class));
+			result.add(mapper.UserToAdvancedUserDto(u));
 		}
 
 		return result;
 	}
-	
+
 	@Override
 	public List<AdvancedUserDto> getAllUsersByType(String type) {
-		if (UserType.contains(type)){
+		if (UserType.contains(type)) {
 			UserType userType = UserType.valueOf(type);
 			List<User> users = userRepository.findAllByType(userType);
 			List<AdvancedUserDto> result = new ArrayList<AdvancedUserDto>();
 			for (User u : users) {
-				result.add(DtoTools.convert(u, AdvancedUserDto.class));
+//				result.add(DtoTools.convert(u, AdvancedUserDto.class));
+				result.add(mapper.UserToAdvancedUserDto(u));
 			}
 			return result;
 		} else {
@@ -78,7 +81,7 @@ public class UserServiceImpl implements UserService {
 		List<AdvancedUserDto> result = new ArrayList<AdvancedUserDto>();
 
 		for (User u : users) {
-			result.add(DtoTools.convert(u, AdvancedUserDto.class));
+			result.add(mapper.UserToAdvancedUserDto(u));
 		}
 		return result;
 	}
@@ -86,7 +89,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public AdvancedUserDto getById(long id) {
 		Optional<User> user = userRepository.findById(id);
-		return DtoTools.convert(user.get(), AdvancedUserDto.class);
+		return mapper.UserToAdvancedUserDto(user.get());
 	}
 
 	@Override
@@ -97,7 +100,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public AdvancedUserDto saveOrUpdate(AdvancedUserDto user) {
 		checkIntegrity(user);
-		
+
 		User u = DtoTools.convert(user, User.class);
 
 		Set<Skill> skillsList = new HashSet<Skill>();
@@ -110,15 +113,15 @@ public class UserServiceImpl implements UserService {
 		u.setSkills(skillsList);
 
 		u.setLocation(locationRepository.getOne(user.getLocationId()));
-		
+
 		// Hash Password
 		try {
 			if (u.getId() == 0) {
+				u.setPassword(HashTools.hashSHA512(u.getPassword()));
+			} else {
+				AdvancedUserDto userInDB = getById(u.getId());
+				if (!userInDB.getPassword().equals(u.getPassword())) {
 					u.setPassword(HashTools.hashSHA512(u.getPassword()));
-				} else {
-					AdvancedUserDto userInDB = getById(u.getId());
-					if (!userInDB.getPassword().equals(u.getPassword())) {
-						u.setPassword(HashTools.hashSHA512(u.getPassword()));
 				}
 			}
 		} catch (Exception e) {
@@ -126,13 +129,13 @@ public class UserServiceImpl implements UserService {
 		}
 
 		u = userRepository.saveAndFlush(u);
-		return DtoTools.convert(u, AdvancedUserDto.class);
+		return mapper.UserToAdvancedUserDto(u);
 	}
-	
+
 	@Override
 	public AdvancedUserDto saveOrUpdatePassword(AdvancedUserDto user) {
 		User u = DtoTools.convert(user, User.class);
-		
+
 		Set<Skill> skillsList = new HashSet<Skill>();
 
 		if (user.getSkillsId() != null) {
@@ -147,26 +150,26 @@ public class UserServiceImpl implements UserService {
 		// Hash Password
 		try {
 			if (u.getId() == 0) {
+				u.setPassword(HashTools.hashSHA512(u.getPassword()));
+			} else {
+				AdvancedUserDto userInDB = getById(u.getId());
+				if (!userInDB.getPassword().equals(u.getPassword())) {
 					u.setPassword(HashTools.hashSHA512(u.getPassword()));
-				} else {
-					AdvancedUserDto userInDB = getById(u.getId());
-					if (!userInDB.getPassword().equals(u.getPassword())) {
-						u.setPassword(HashTools.hashSHA512(u.getPassword()));
 				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		u = userRepository.saveAndFlush(u);
-		return DtoTools.convert(u, AdvancedUserDto.class);
-			
-}
+		return mapper.UserToAdvancedUserDto(u);
+
+	}
 
 	@Override
 	public AdvancedUserDto findByEmail(String email) {
 		User u = userRepository.findByEmail(email);
 		if (u != null)
-			return DtoTools.convert(u, AdvancedUserDto.class);
+			return mapper.UserToAdvancedUserDto(u);
 
 		return null;
 	}
@@ -175,62 +178,55 @@ public class UserServiceImpl implements UserService {
 	public long count() {
 		return userRepository.count();
 	}
-	
+
 	public boolean checkIntegrity(AdvancedUserDto u) {
 		Set<APIError> errors = new HashSet<APIError>();
 		String instanceClass = u.getClass().toString();
 		String path = "/api/users";
-		//Location Must EXIST
+		// Location Must EXIST
 		if (!locationRepository.findById(u.getLocationId()).isPresent()) {
 			String message = "Location with id: " + u.getLocationId() + " does not exist.";
-			errors.add(new APIError(301, instanceClass, "LocationNotFound",
-					message, path));
+			errors.add(new APIError(301, instanceClass, "LocationNotFound", message, path));
 		}
-		
-		//IF Skill > Must EXIST
+
+		// IF Skill > Must EXIST
 		if (u.getSkillsId() != null) {
 			for (long skillId : u.getSkillsId()) {
-				if(!skillRepository.findById(skillId).isPresent()) {
+				if (!skillRepository.findById(skillId).isPresent()) {
 					String message = "Skill with id: " + skillId + " does not exist.";
-					errors.add(new APIError(302, instanceClass, "SkillNotFound",
-							message, path));
+					errors.add(new APIError(302, instanceClass, "SkillNotFound", message, path));
 				}
 			}
 		}
-		
-		//Email > valid, uniq
+
+		// Email > valid, uniq
 		if (!User.emailIsValid(u.getEmail())) {
 			String message = "Email must be valid.";
-			errors.add(new APIError(303, instanceClass, "InvalidEmail",
-					message, path));
+			errors.add(new APIError(303, instanceClass, "InvalidEmail", message, path));
 		}
-		
-		if(userRepository.findDuplicateEmail(u.getEmail(), u.getId()) != null) {
+
+		if (userRepository.findDuplicateEmail(u.getEmail(), u.getId()) != null) {
 			String message = "Email already used.";
-			errors.add(new APIError(304, instanceClass, "EmailNotUniq",
-					message, path));
+			errors.add(new APIError(304, instanceClass, "EmailNotUniq", message, path));
 		}
-		
-		//password > 8 char min
-		if(u.getPassword().length() < 8) {
+
+		// password > 8 char min
+		if (u.getPassword().length() < 8) {
 			String message = "Password must be at least 8 characters long";
-			errors.add(new APIError(305, instanceClass, "PasswordTooShort",
-					message, path));
+			errors.add(new APIError(305, instanceClass, "PasswordTooShort", message, path));
 		}
-		
+
 		// Company + type enums must be valid
 		if (!UserCompany.contains(u.getCompany())) {
 			String message = "Company: " + u.getCompany() + " is not valid.";
-			errors.add(new APIError(306, instanceClass, "UnknownUserCompany",
-					message, path));
+			errors.add(new APIError(306, instanceClass, "UnknownUserCompany", message, path));
 		}
-		
+
 		if (!UserType.contains(u.getType())) {
 			String message = "Type: " + u.getType() + " is not valid.";
-			errors.add(new APIError(307, instanceClass, "UnknownUserType",
-					message, path));
+			errors.add(new APIError(307, instanceClass, "UnknownUserType", message, path));
 		}
-		//If Image > must exist
+		// If Image > must exist
 		if (!errors.isEmpty()) {
 			throw new EntityFormatException(errors);
 		}
