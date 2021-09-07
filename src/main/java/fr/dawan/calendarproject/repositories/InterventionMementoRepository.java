beb741b465db701582a598deeed48211884481ea
Romain Maury <rmaury@dawan.fr>
2021-09-07 10:31:17 +0200

    removed bad query

--- src/main/java/fr/dawan/calendarproject/repositories/InterventionMementoRepository.java
+++ src/main/java/fr/dawan/calendarproject/repositories/InterventionMementoRepository.java
@@ -17,13 +17,4 @@ public interface InterventionMementoRepository extends JpaRepository<Interventio
 	// get the last intervention memento
 	@Query(nativeQuery = true, value = "SELECT * FROM Intervention_Memento i WHERE i.intervention_id = :interventionId ORDER BY i.date_created_state DESC LIMIT 1")
 	InterventionMemento getLastInterventionMemento(@Param("interventionId") long interventionId);
-	
-	@Query("FROM InterventionMemento i LEFT JOIN FETCH i.state.locationId LEFT JOIN FETCH i.state.userId LEFT JOIN FETCH i.state.courseId WHERE i.id = :id")
-	InterventionMemento findById2(@Param("id") long id);
 }
-
-//SELECT * FROM `intervention_memento` i 
-//	INNER JOIN `location` l ON i.location_id = l.id 
-//	INNER JOIN `course` c ON i.course_id = c.id 
-//	INNER JOIN `user` u ON i.user_id = u.id 
-//	WHERE i.id = 79
