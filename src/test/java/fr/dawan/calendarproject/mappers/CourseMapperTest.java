aa313bdc7284ee03b18664e163292bbdac46ba7c
Ahmed Reda Mokhtari <amokhtari@dawan.fr>
2021-09-08 16:12:31 +0200

    add CourseMapperTest

---
+++ src/test/java/fr/dawan/calendarproject/mappers/CourseMapperTest.java
@@ -0,0 +1,57 @@
+package fr.dawan.calendarproject.mappers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import fr.dawan.calendarproject.dto.CourseDto;
+import fr.dawan.calendarproject.entities.Course;
+import fr.dawan.calendarproject.mapper.CourseMapper;
+import fr.dawan.calendarproject.services.CourseService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CourseMapperTest {
+
+	@Autowired
+	CourseService courseService;
+
+	@Autowired
+	CourseMapper courseMapper;
+
+	Course course = new Course();
+	CourseDto courseDto = new CourseDto();
+
+	@BeforeEach
+	void before() {
+		courseDto = new CourseDto(1, "title", 3);
+		course = new Course(3, "eltit", 1);
+	}
+
+	@Test
+	void should_map_courseToCourseDto() {
+		// mapping
+		CourseDto mappedCourseDto = courseMapper.courseToCourseDto(course);
+
+		// assert
+		assertEquals(mappedCourseDto.getId(), course.getId());
+		assertEquals(mappedCourseDto.getTitle(), course.getTitle());
+		assertEquals(mappedCourseDto.getVersion(), course.getVersion());
+	}
+
+	@Test
+	void should_map_courseDtoToCouse() {
+		// mapping
+		Course mappedCourse = courseMapper.courseDtoToCouse(courseDto);
+
+		// assert
+		assertEquals(mappedCourse.getId(), courseDto.getId());
+		assertEquals(mappedCourse.getTitle(), courseDto.getTitle());
+		assertEquals(mappedCourse.getVersion(), courseDto.getVersion());
+	}
+
+}
