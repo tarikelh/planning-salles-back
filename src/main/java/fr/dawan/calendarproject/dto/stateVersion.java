58a3399ac74be7182d33022fedb238e5a9a2d584
jdasilvalima <jade.dasilva.lima.jds@gmail.com>
2021-09-29 12:21:45 +0200

    added object for getAll

---
+++ src/main/java/fr/dawan/calendarproject/dto/stateVersion.java
@@ -0,0 +1,14 @@
+package fr.dawan.calendarproject.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class stateVersion {
+	
+	private long version;
+	
+	private LocalDateTime timeStamp;
+	
+	private List<Long> deleteItems;
+	
+}
