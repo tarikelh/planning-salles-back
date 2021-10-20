68e5a363c8892fb053bd067e3a1288d4911fc9c6
Ahmed Reda Mokhtari <amokhtari@dawan.fr>
2021-10-20 16:37:21 +0200

    test corss origine

--- src/main/java/fr/dawan/calendarproject/controllers/LocationController.java
+++ src/main/java/fr/dawan/calendarproject/controllers/LocationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import fr.dawan.calendarproject.dto.CountDto;
 import fr.dawan.calendarproject.dto.LocationDto;
