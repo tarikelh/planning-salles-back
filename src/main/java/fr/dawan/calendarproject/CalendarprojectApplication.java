afff873017bb903ace9221a53667f9003d51d903
Jade Da Silva Lima <jdasilva@dawan.fr>
2021-10-20 15:05:54 +0200

    Update allowedHeaders CalendarprojectApplication.java

--- src/main/java/fr/dawan/calendarproject/CalendarprojectApplication.java
+++ src/main/java/fr/dawan/calendarproject/CalendarprojectApplication.java
@@ -31,7 +31,7 @@ public class CalendarprojectApplication {
 			// CROSS ORIGIN
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*");
+				registry.addMapping("/**").allowedHeaders("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*");
 			}
 
 			// CONVERTERS
