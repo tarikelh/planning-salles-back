0e6be98d844540b7d4f444d17a791de4d1c516fb
jdasilvalima <jade.dasilva.lima.jds@gmail.com>
2021-11-05 16:44:43 +0100

    added course test & compare + cvs tests

---
+++ src/test/java/fr/dawan/calendarproject/services/InterventionCaretakerTest.java
@@ -0,0 +1,69 @@
+package fr.dawan.calendarproject.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InterventionCaretakerTest {
+
+	@Test
+	void testAddMemento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMementoById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllMemento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllMementoIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllMementoDates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRestoreMemento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSerializeInterventionMementosAsCSV() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSerializeInterventionMementosAsCSVByDates() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFilterMemento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCountFilter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLastBeforeMemento() {
+		fail("Not yet implemented");
+	}
+
+}
