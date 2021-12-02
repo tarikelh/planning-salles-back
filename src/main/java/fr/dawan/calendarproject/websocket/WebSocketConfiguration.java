6000c56cc43088093974ae71989fb68bfcd9ea04
Jade Da Silva Lima <jdasilva@dawan.fr>
2021-12-02 20:58:39 +0000

    Update WebSocketConfiguration.java

--- src/main/java/fr/dawan/calendarproject/websocket/WebSocketConfiguration.java
+++ src/main/java/fr/dawan/calendarproject/websocket/WebSocketConfiguration.java
@@ -54,7 +54,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		// registry.addEndpoint("/ws");
 		registry.addEndpoint("/ws")
-				// Allow the origin http://localhost:8080 to send messages to us. (Base URL of
+				// Allow the origin http://server:port to send messages to us. (Base URL of
 				// the client)
 				.setAllowedOrigins(vueUrl)
 				// Enable SockJS fallback options
