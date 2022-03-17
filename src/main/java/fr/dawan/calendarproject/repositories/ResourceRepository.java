8a73ad2bb52a89c590f5a7807fc50885f52516c9
Alaaeddine <vincentfontaine.jobs@gmail.com>
2022-03-17 13:02:47 +0100

    repostory fini (Booking - ressource)

---
+++ src/main/java/fr/dawan/calendarproject/repositories/ResourceRepository.java
@@ -0,0 +1,18 @@
+package fr.dawan.calendarproject.repositories;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import io.github.classgraph.Resource;
+
+@Repository	
+public interface ResourceRepository extends JpaRepository<Resource,Long> {
+
+	 @Query("FROM Resource r Where r.name = :name")
+	    List<Resource> findByname(@Param("name") String name);
+	 
+
+}
