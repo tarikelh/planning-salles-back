60b678bef32b85695061284e68a25ac8a815481b
Lionel L <lourenco.lionel@hotmail.fr>
2022-09-21 12:23:38 +0200

    Created Interface For Task Service

---
+++ src/main/java/fr/dawan/calendarproject/services/TaskService.java
@@ -0,0 +1,27 @@
+package fr.dawan.calendarproject.services;
+
+import java.util.List;
+
+import fr.dawan.calendarproject.dto.CountDto;
+import fr.dawan.calendarproject.dto.TaskDto;
+
+public interface TaskService {
+	
+	
+	List<TaskDto> getAllTask();
+	
+	List<TaskDto> getAllTaskForUserId(long userId);
+	
+	List<TaskDto> getAllTaskForInternventionId(long interventionId);
+	
+	List<TaskDto> getAllBySlugLike(String search);
+	
+	TaskDto getTaskById();
+	
+	CountDto count(String search);
+	
+	TaskDto saveOrUpdate(TaskDto taskDto);
+	
+	void deleteById(long id);
+
+}
