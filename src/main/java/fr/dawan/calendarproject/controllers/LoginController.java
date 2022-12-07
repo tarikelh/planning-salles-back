62bf417e74aede476638bc5753ff3a9a71fc51fc
Lionel L <lourenco.lionel@hotmail.fr>
2022-12-07 16:47:04 +0100

    Uncommented method to fetch from dg2

--- src/main/java/fr/dawan/calendarproject/controllers/LoginController.java
+++ src/main/java/fr/dawan/calendarproject/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package fr.dawan.calendarproject.controllers;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -14,11 +16,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import fr.dawan.calendarproject.dto.LoginDto;
 import fr.dawan.calendarproject.dto.LoginResponseDto;
@@ -216,23 +222,26 @@ public class LoginController {
 //		}
 //	}
 
-//	@PostMapping(value = "/test-dg2-login", consumes = "application/json")
-//	public ResponseEntity<Boolean> testDg2LogIn(@RequestBody LoginDto loginObj) {
-//		URI url = null;
-//
-//		try {
-//			url = new URI("https://dawan.org/api2/planning/locations");
-//		} catch (URISyntaxException e) {
-//			e.printStackTrace();
-//		}
-//
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.add("x-auth-token", loginObj.getEmail() + ":" + loginObj.getPassword());
-//
-//		HttpEntity<String> httpEntity = new HttpEntity<>(headers);
-//
-//		ResponseEntity<String> repWs = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
-//
-//		return ResponseEntity.ok((repWs.getStatusCode() == HttpStatus.OK));
-//	}
+	@PostMapping(value = "/test-dg2-login", consumes = "application/json")
+	public ResponseEntity<Boolean> testDg2LogIn(@RequestBody LoginDto loginObj) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		URI url = null;
+
+		try {
+			url = new URI("https://dawan.org/api2/planning/locations");
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("x-auth-token", loginObj.getEmail() + ":" + loginObj.getPassword());
+
+		HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+		ResponseEntity<String> repWs = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+
+		return ResponseEntity.ok((repWs.getStatusCode() == HttpStatus.OK));
+	}
 }
